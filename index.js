d3ee53d74dbbfdd7dc18c346f5f9c1a2c936167f
SemenSavlook <semensavluk@gmail.com>
2026-05-28 16:50:36 +0500

    1-51-00 add Game timer

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
-// 1-02-04
 
 canvas.width = 1024;
 canvas.height = 576;
@@ -143,6 +142,37 @@ function rectangularCollision({rectangle1, rectangle2}) {
   )
 }
 
+function determineWinner({player, enemy, timerID}) {
+  clearTimeout(timerID);
+  document.querySelector('#displayText').style.display = 'flex';
+
+  if (player.health === enemy.health) {
+    document.querySelector('#displayText').innerHTML = 'Tie';
+  } else if (player.health > enemy.health) {
+    document.querySelector('#displayText').innerHTML = 'Player 1 Wins';
+  } else if (player.health < enemy.health) {
+    document.querySelector('#displayText').innerHTML = 'Player 2 Wins';
+  }
+}
+
+
+  // Time control
+let timer = 60;
+var timerID;
+function decreaseTimer() {
+  timerID = setTimeout(decreaseTimer, 1000);
+  if (timer > 0) {
+    timer--;
+    document.querySelector('#timer').innerHTML = timer;
+  }
+  
+  if (timer === 0) {
+    determineWinner({ player, enemy, timerID });
+  }
+
+}
+
+decreaseTimer()
 
 function animate() {
   window.requestAnimationFrame(animate);
@@ -187,6 +217,11 @@ function animate() {
     playerHealthEl.style.width = player.health + '%'
   }
 
+
+  // end game based on health
+  if (enemy.health <= 0 || player.health <= 0) {
+    determineWinner({ player, enemy, timerID });
+  }
 }
 
 animate();
