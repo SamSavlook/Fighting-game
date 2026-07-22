d2775d5dd6e22a9e605fd929e09dd5c04b5e7a66
SemenSavlook <semensavluk@gmail.com>
2026-07-22 18:49:25 +0500

    2-23-11 add sprite

--- index.js
+++ index.js
@@ -19,6 +19,16 @@ const background = new Sprite({
   imageSrc: './img/background.png'
 });
 
+const shop = new Sprite({
+  position: {
+    x: 600,
+    y: 128
+  },
+  imageSrc: './img/shop.png',
+  scale: 2.75,
+  framesMax: 6
+});
+
 //--------------------- Players ---------------------
 
 const player = new Fighter({
@@ -84,6 +94,7 @@ function animate() {
   c.fillStyle = 'black';
   c.fillRect(0, 0, canvas.width, canvas.height);
   background.update();
+  shop.update();
   player.update();
   enemy.update();
 
